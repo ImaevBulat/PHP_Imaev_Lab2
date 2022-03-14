fcdd09fe9f1250b27fda455da513e8c6319ce7aa
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-14 21:05:17 +0500

    Выполнил задание 2-4 вариант 12

---
+++ lab-2-4-2.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>Imaev Bulat</title></head>
+</html>
+
+<?php
+$n = 20; $b = 10; $d = 19;
+echo 'Ischodniy: <br />';
+for($i = 0; $i < $n; $i++) {
+$a[$i] = rand(-10, 10);
+echo $a[$i].'&nbsp';
+}
+echo '<br />Skorrectirovanniy: <br />';
+for($i = 0; $i < $n; $i++) {
+if (($i < $b || $i > $d) && $a[$i] >= 0) $a[$i] = 1;
+echo $a[$i].'&nbsp';
+}
+?>
